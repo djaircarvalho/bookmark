7fa5b641ef73f2395473c7d614e06cfc01aadf09
Djair Carvalho <djaircarvalho.dj7@gmail.com>
2016-11-15 07:08:27 -0300

    Adicionado remover bookmark e modificado aparencia dos bookmarkers na pagina de bookmarks

--- app/js/bookmarks.controller.js
+++ app/js/bookmarks.controller.js
@@ -1,30 +1,50 @@
 app.controller('bookmarksController', function ($http, $scope) {
   $scope.bookmarks = [];
-  var baseUrl = 'http://localhost:3000/bookmarks';
+  var baseUrl = 'http://localhost:3000/bookmarks/';
   var config = {
       headers: {"Content-Type": "application/json", "Accept": "application/json",
-      "Authorization":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE0NzkxNDc0Nzh9.NjnorgFvNX8gQP2lWgikB8hgT3f0_FC9Mmb2ix0oukM"
+      "Authorization":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0LCJleHAiOjE0NzkyNTc2NjR9.WnL2Eo8Al4iqb_J59SXphwXrekHbna0TVc45N3v2Kak"
       }
-    }
-  $http.get(baseUrl, config)
-    .then(function(response) {
-      $scope.bookmarks = response.data;
-    }, function(err) {
-      console.log(err);
-    });
+    };
+
+
+  var load = function () {
+    $http.get(baseUrl, config)
+      .then(function(response) {
+        $scope.bookmarks = response.data;
+      }, function(err) {
+        console.log(err);
+      });
+  } ;
+
+
+
   $scope.addBookmark = function () {
-    data = { bookmark:  $scope.bookmark   }
+    data = { bookmark:  $scope.bookmark   };
     console.log(data);
 
     $http.post(baseUrl, data, config)
           .success(function (data, status, headers, config) {
-            $scope.success = true
+            $scope.success = true;
+            $scope.bookmark = {};
           })
           .error(function (data, status, header, config) {
             $scope.error = data.error;
           });
-
-      console.log($scope.bookmarks);
   };
+  $scope.removeBookmark = function (id) {
+    var url= baseUrl + id;
+    $http.delete(url, config)
+     .then(
+         function(response){
+         load();
+         },
+         function(response){
+           console.log("fail");
+         }
+      );
+  };
+
+  load();
 
 });
--- app/js/main.js
+++ app/js/main.js
@@ -39,25 +39,13 @@ var app = angular.module('app', ['ngRoute']);
               templateUrl:'view/user/signin.html',
               controller:'userController'
             })
-
-            .when('/logout',{
-              templateUrl:'view/user/logout.html',
-              controller:'userController'
-            })
-
           	.otherwise({redirectTo:'/'});
     });
 
     // create the controller and inject Angular's $scope
     app.controller('mainController', function($scope) {
         // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
-    });
-
-    app.controller('aboutController', function($scope) {
-        $scope.message = 'Look! I am an about page.';
-    });
-
-    app.controller('contactController', function($scope) {
-        $scope.message = 'Contact us! JK. This is just a demo.';
+        $scope.logout = function () {
+          console.log('Everyone come and see how good I look!');
+        };
     });
